81e223fc4d7f49525a2fe871a9e01b508b778046
Lil Shitty <pootogether@tutanota.com>
2020-11-15 22:53:41 +0200

    deploy POO

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,10 @@
+const POO = artifacts.require("POO");
 const PooTogether = artifacts.require("PooTogether");
 const Distributor = artifacts.require("Distributor");
 
 module.exports = async function (deployer) {
+	const yusdVault = "0x5dbcF33D8c2E976c6b560249878e6F1491Bca25c"
+	await deployer.deploy(POO, { gas: 1200000 });
 	await deployer.deploy(Distributor, { gas: 600000 });
-	await deployer.deploy(PooTogether, "0x5dbcF33D8c2E976c6b560249878e6F1491Bca25c", Distributor.address, { gas: 3000000 });
+	await deployer.deploy(PooTogether, yusdVault, Distributor.address, { gas: 2600000 });
 };
