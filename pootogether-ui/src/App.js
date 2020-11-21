6de6c5dfcdc1ef402558a07fea095c6db5b3cd07
Lil Shitty <pootogether@tutanota.com>
2020-11-21 20:25:55 +0200

    just initial stuff

--- pootogether-ui/src/App.js
+++ pootogether-ui/src/App.js
@@ -1,34 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
-import ethers from 'ethers';
-import { Contract, getDefaultProvider } from 'ethers';
-
+import { Contract, getDefaultProvider, BigNumber, utils } from 'ethers'
+import { useState, useEffect } from 'react'
+const { formatUnits, parseUnits } = utils
 const provider = getDefaultProvider('homestead')
 
-//const POO = new Contract('0x6A54EF1680f593574522422f3700194EC91CE57d', require('./interfaces/ERC20').abi, provider)
-const PooTogether = new Contract('0x19a62938f67F2A44C47975Cc4c1132B7B75Aab76', require('./interfaces/PooTogether').abi, provider)
+//const POO = new Contract('0x6A54EF1680f593574522422f3700194EC91CE57d', require('./interfaces/ERC20'), provider)
+const PooTogether = new Contract('0x19a62938f67F2A44C47975Cc4c1132B7B75Aab76', require('./interfaces/PooTogether'), provider)
+const Vault = new Contract('0x5dbcF33D8c2E976c6b560249878e6F1491Bca25c', require('./interfaces/Vault'), provider)
 
-// const Vault
+// getPricePerFullShare
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [stats, setStats] = useState({ staked: BigNumber.from(0) })
+
+	useEffect(() => {
+		(async () => {
+			setStats({ staked: await Vault.balanceOf(PooTogether.address) })
+		})()
+	}, [])
+
+	return (
+		 <div className="App">
+			<header className="App-header">
+			  <p>{formatUnits(stats.staked, 18)}</p>
+			  <a
+				 className="App-link"
+				 href="https://reactjs.org"
+				 target="_blank"
+				 rel="noopener noreferrer"
+			  >
+				 Learn React
+			  </a>
+			</header>
+		 </div>
+	);
 }
 
 export default App;
